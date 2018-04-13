0410442b76ce2c1aab1d265f1b02b538c5d708cc
Randy Fernandez <randy.fernandez@datalynx.ch>
2018-04-12 22:40:48 -0600

    Test route

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
